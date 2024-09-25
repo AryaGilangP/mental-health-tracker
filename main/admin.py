b58eedcf535102227873ca08a681a0d3d1f35a9d
AryaGilangP <aryagilang05@gmail.com>
2024-09-25 15:32:34 +0700

    tutorial 4

--- main/admin.py
+++ main/admin.py
@@ -3,3 +3,7 @@ from django.contrib import admin
 # Register your models here.
 # arya.gilang = abcdef123456
 # gilang.bingung = 123456abcdef
+# bukan.gilang = bukanabcdef123456
+
+# Username: arya.gilang
+# Password: DAF83bxANuuj7mmx0GaNXyFRMgo-Zr4Y
